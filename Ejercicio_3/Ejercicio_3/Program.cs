72d6fb99443bd5cb8befa978f77f9d103b4964f7
chris <ipanaquediazcesa@gmail.com>
2026-04-16 19:01:52 -0500

    Tercer

---
+++ Ejercicio_3/Ejercicio_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el peso en KG: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la estatura en metros: ");
+            double estatura = double.Parse(Console.ReadLine());
+
+            double imc = peso / Math.Pow(estatura, 2);
+
+            double imcRed = Math.Round(imc, 2);
+
+            Console.WriteLine($"IMC: {imcRed}");
+        }
+    }
+}
