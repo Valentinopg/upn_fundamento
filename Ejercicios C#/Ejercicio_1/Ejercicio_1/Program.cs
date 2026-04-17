3c8b11894c160b7768167503c6bc8586b487dd89
Valentino <N00502022@upn.pe>
2026-04-17 17:32:36 -0500

    Agregando Ejercicios

---
+++ Ejercicios C#/Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Ingrese el monto de la compra (S/): ");
+double monto = double.Parse(Console.ReadLine());
+
+double descuento = 0;
+
+if (monto >=100)
+{
+    descuento = monto * 0.10;
+}
+
+double total = monto - descuento;
+
+Console.WriteLine($"Descuento aplicado: S/ {descuento:F2}");
+Console.WriteLine($"Total a pagar:      S/ {total:F2}");
